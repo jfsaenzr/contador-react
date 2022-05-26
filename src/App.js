2cba9a8384381a2eaf716a83ea2267c02e875cce
jfsaenzr <47581366+jfsaenzr@users.noreply.github.com>
2022-05-26 15:27:30 -0500

    *

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import logo from './imganes/logo.png';
 
 function App() {
+
+  const manejarClick = () => {
+    console.log('Click')
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar')
+  }
+
   return (
     <div className="App">
-
+      <div className='logo-contenedor'>
+        <img
+          className='logo'
+          src={logo}
+          alt='logo'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Boton
+          texto='Click'
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
